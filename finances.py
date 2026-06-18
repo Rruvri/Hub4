7daee109ce4b8bb0b0bce078e5a17480fac1d815
Rruvri <ravinathwani@btopenworld.com>
2026-06-18 13:25:47 +0100

    added pyqt imports to all relevant .py files

--- finances.py
+++ finances.py
@@ -1,4 +1,15 @@
-
+import sys
+from PyQt6.QtWidgets import (QApplication, QLabel,
+                             QWidget, QGridLayout,
+                             QFormLayout, QLineEdit,
+                             QDialog, QHBoxLayout,
+                             QDialogButtonBox, QMainWindow,
+                             QStackedLayout, QComboBox,
+                             QCalendarWidget, QDateEdit,
+                             QDateTimeEdit, QCheckBox,
+                             QVBoxLayout, QListWidget, 
+                             QPushButton)
+from PyQt6.QtCore import (QDate, QDateTime)
 
 current_account_balances = {}
 
--- tasks.py
+++ tasks.py
@@ -1,3 +1,17 @@
+import sys
+from PyQt6.QtWidgets import (QApplication, QLabel,
+                             QWidget, QGridLayout,
+                             QFormLayout, QLineEdit,
+                             QDialog, QHBoxLayout,
+                             QDialogButtonBox, QMainWindow,
+                             QStackedLayout, QComboBox,
+                             QCalendarWidget, QDateEdit,
+                             QDateTimeEdit, QCheckBox,
+                             QVBoxLayout, QListWidget, 
+                             QPushButton)
+from PyQt6.QtCore import (QDate, QDateTime)
+
+
 open_tasks = {}
 
 tasks_archive = {}
